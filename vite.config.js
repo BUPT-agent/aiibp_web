fe08e357b4728ccd200ff193cf8d415ecc2c4784
uglyghost <chenxingyan@bupt.edu.cn>
2025-11-30 21:15:44 +0800

    github online

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ⚠️ 这里必须替换为你的仓库名，前后都要有斜杠
-  base: '/ai-interview-bp/',
+  // ⚠️ 注意：这里必须是 '/aiibp_web/'，因为你的仓库名是 aiibp_web
+  base: '/aiibp_web/',
 })
